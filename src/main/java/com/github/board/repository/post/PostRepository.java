7a89a2ae32983ebbebc9a85dbd24f1d5c169244f
zzoeun <pje308@naver.com>
2024-12-13 03:16:09 +0900

    add PostRepository

---
+++ src/main/java/com/github/board/repository/post/PostRepository.java
@@ -0,0 +1,19 @@
+package com.github.movieproject.repository.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface PostRepository extends JpaRepository<Posts, Long> {
+
+    Optional<Posts> findByIdx(Integer idx);
+
+    @Query("SELECT p FROM Posts p JOIN FETCH p.user u WHERE u.email = :email")
+    List<Posts> findByUserEmail(@Param("email") String email);
+
+    @Query("SELECT p FROM Posts p JOIN FETCH p.user u")
+    List<Posts> findAllPost();
+}
--- src/main/java/com/github/board/repository/post/Posts.java
+++ src/main/java/com/github/board/repository/post/Posts.java
@@ -1,4 +1,40 @@
 package com.github.board.repository.post;
 
+
+import com.github.movieproject.repository.auth.user.Users;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Builder
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@DynamicInsert
+@Table(name="posts")
+
 public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 자동 생성
+    private Integer idx;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String body;
+
+    @ManyToOne(fetch = FetchType.EAGER) // 즉시 로딩
+    @JoinColumn(name = "user_idx", nullable = false)
+    private Users user;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime created_at = LocalDateTime.now();
 }
