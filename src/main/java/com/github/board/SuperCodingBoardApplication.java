9f620bdf802d8fe853553c9f76834fd338f029d5
kdh6194 <kdh6194@naver.com>
2024-12-12 21:06:21 +0900

    first commit

---
+++ src/main/java/com/github/board/SuperCodingBoardApplication.java
@@ -0,0 +1,13 @@
+package com.github.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCodingBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperCodingBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/board/SuperCodingBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperCodingBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
